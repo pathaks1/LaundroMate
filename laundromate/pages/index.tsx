5e5e7100252580e8e0473fed404f936295249914
Shivam Pathak <shivampathak108@outlook.com>
2023-09-24 02:00:00 -0500

    trying once again

--- laundromate/pages/index.tsx
+++ laundromate/pages/index.tsx
@@ -40,7 +40,7 @@ export async function getServerSideProps(ctx:any) {
     if (session) {
         return {
             redirect: {
-                destination: `/machines`,
+                destination: '/machines',
                 permanent: false
             }
         }
