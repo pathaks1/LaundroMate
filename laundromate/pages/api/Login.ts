3f36dbc04237bec0c9264de627c5c8d3b9a093e2
Shivam Pathak <shivampathak108@outlook.com>
2023-09-24 01:35:33 -0500

    retrying redirect

--- laundromate/pages/api/Login.ts
+++ laundromate/pages/api/Login.ts
@@ -1,17 +1,17 @@
 import type { SupabaseClient } from "@supabase/auth-helpers-react"
 import { createClient } from '@supabase/supabase-js';
 
-const getURL = () => {
-    let url =
-        process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-        process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-        "http://laundro-mate.vercel.app//machines"
-    // Make sure to include `https://` when not localhost.
-    url = url.includes("http") ? url : `https://${url}`
-    // Make sure to including trailing `/`.
-    url = url.charAt(url.length - 1) === "/" ? url : `${url}/`
-    return `${url}`
-}
+// const getURL = () => {
+//     let url =
+//         process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+//         process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+//         "http://laundro-mate.vercel.app//machines"
+//     // Make sure to include `https://` when not localhost.
+//     url = url.includes("http") ? url : `https://${url}`
+//     // Make sure to including trailing `/`.
+//     url = url.charAt(url.length - 1) === "/" ? url : `${url}/`
+//     return `${url}`
+// }
 
 export const handleLogin = async (
     supabase: SupabaseClient<any, "public", any>
@@ -19,7 +19,7 @@ export const handleLogin = async (
     const res = await supabase.auth.signInWithOAuth({
         provider: "google",
         options: {
-            redirectTo: getURL(),
+            redirectTo: "/machines",
         },
     })
 }
